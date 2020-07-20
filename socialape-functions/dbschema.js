1f72dce18bc068706bd3044094fd1767db008c1b
Anton <a0660576041@gmail.com>
2020-07-20 11:26:55 +0300

    initialize project. firebase configuration

---
+++ socialape-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2020-07-18T16:12:31.150Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
