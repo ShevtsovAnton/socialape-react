d3087fc2f84361a3616cc8cdea5b49e7d3552638
Anton <a0660576041@gmail.com>
2020-07-20 16:21:03 +0300

    feat: add authentication middleware

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -32,11 +32,38 @@ app.get('/screams', (req, res) => {
         .catch(err => console.error(err));
 })
 
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found');
+        return req.status(403).json({ error: 'Unauthorized' })
+    }
 
-app.post('/scream', (req, res) => {
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get()
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token', err);
+            return res.status(403).json(err);
+        })
+}
+
+//Post one scream
+app.post('/scream', FBAuth, (req, res) => {
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     }
     db
