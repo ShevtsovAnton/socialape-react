25b84d9ce3960535b79a05f80ea465883b4c051a
Anton <a0660576041@gmail.com>
2020-07-22 17:00:43 +0300

    fix: change error handle signup and login

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -57,7 +57,7 @@ exports.signUp = (req, res) => {
             if (err.code === 'auth/email-already-in-use') {
                 return res.status(400).json({ email: 'email already in use' })
             } else {
-                return res.status(500).json({ error: err.code });
+                return res.status(500).json({ general: 'Something went wrong, please try again' });
             }
         })
 }
@@ -80,9 +80,7 @@ exports.login = (req, res) => {
         })
         .catch(err => {
             console.error(err);
-            if (err.code === 'auth/wrong-password') {
-                return res.status(403).json({ general: 'Wrong credentials, please try again' })
-            }
+            return res.status(403).json({ general: 'Wrong credentials, please try again' })
             return res.status(500).json({ error: err.code });
         })
 
