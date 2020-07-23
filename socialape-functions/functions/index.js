cdadf3cd8917dd33f002d0da75e02cbd15189e8d
Anton <a0660576041@gmail.com>
2020-07-23 12:10:58 +0300

    feat: clean up in notifications, likes, comments, after the scream is deleted

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -98,3 +98,60 @@ exports.deleteNotificationOnUnLike = functions
             })
 
     })
+
+exports.onUserImageChange = functions
+    .region('europe-west1')
+    .firestore.document('/users/{userId}')
+    .onUpdate((change) => {
+        if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+            const batch = db.batch();
+            return db
+                .collection('screams')
+                .where('userHandle', '==', chage.before.data().handle)
+                .get()
+                .then((data) => {
+                    data.forEach(doc => {
+                        const scream = db.doc(`/screams/${doc.id}`);
+                        batch.update(scream, { userImaga: change.after.data().imageUrl });
+                    })
+                    return batch.commit();
+                })
+        } else return true;
+    })
+
+exports.onScreamDelete = functions
+    .region('europe-west1')
+    .firestore.document('/screams/{screamId}')
+    .onDelete((snapshot, context) => {
+        const screamId = context.params.screamId;
+        const batch = db.batch();
+        return db
+            .collection('comments')
+            .where('screamId', '==', screamId)
+            .get()
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`/comments/${doc.id}`));
+                })
+                return db
+                    .collection('likes')
+                    .where('screamId', '==', screamId).get();
+            })
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`/likes/${doc.id}`));
+                })
+                return db
+                    .collection('notifications')
+                    .where('screamId', '==', screamId).get();
+            })
+            .then(data => {
+                data.forEach(doc => {
+                    batch.delete(db.doc(`/notifications/${doc.id}`));
+                })
+                return batch.commit();
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    })
