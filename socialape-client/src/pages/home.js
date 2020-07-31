294edd66b050f3af8bc45b8d541048bd489cba05
Anton <a0660576041@gmail.com>
2020-07-31 14:27:46 +0300

    feat: Image upload

--- socialape-client/src/pages/home.js
+++ socialape-client/src/pages/home.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-import axios from "axios";
+import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
 
 import Scream from '../components/Scream';
-import Profile from '../components/Profile'
+import Profile from '../components/Profile';
 
 export class home extends Component {
   state = {
     screams: null,
   };
+
+  _isMounted = false;
+
   componentDidMount() {
+    this._isMounted = true;
     axios
       .get('/screams')
       .then((res) => {
-        this.setState({
-          screams: res.data,
-        });
+        if (this._isMounted) {
+          this.setState({
+            screams: res.data,
+          });
+        }
       })
       .catch((err) => console.log(err));
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
   render() {
